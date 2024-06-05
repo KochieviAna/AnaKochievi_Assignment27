575f648478f02442ca69e125e175683c2877bf80
KochieviAna <anna.kochievi@gmail.com>
2024-06-06 01:29:40 +0400

    Initial Commit

---
+++ AnaKochievi_Assignment27/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnaKochievi_Assignment27
+//
+//  Created by MacBook on 06.06.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
